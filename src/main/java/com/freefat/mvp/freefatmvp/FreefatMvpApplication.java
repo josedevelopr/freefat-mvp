634b74bd3b33f8aff1499c036381d03d42577576
jose antonio alvino velasque <jose_antonio_alvino21@outlook.es>
2020-11-05 23:51:16 -0500

    Initial commit

---
+++ src/main/java/com/freefat/mvp/freefatmvp/FreefatMvpApplication.java
@@ -0,0 +1,13 @@
+package com.freefat.mvp.freefatmvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreefatMvpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreefatMvpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/freefat/mvp/freefatmvp/FreefatMvpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freefat.mvp.freefatmvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreefatMvpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
